5ea59130218b5402d6f7892c551dd3d4776a93a2
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-11-09 20:54:04 +0200

    branch practice 1

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,7 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        c_dict = toml.loads(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(c_dict['tool']['poetry']['name'], c_dict['tool']['poetry']['description'], c_dict['tool']['poetry']['license'], c_dict['tool']['poetry']['authors'], list(c_dict['tool']['poetry']['dependencies'].keys()), list(c_dict['tool']['poetry']['group']['dev']['dependencies'].keys()))
