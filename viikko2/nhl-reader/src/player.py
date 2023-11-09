9ab7559ee63a2b28185a0a8ded433b05b246eed8
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-11-10 00:04:16 +0200

    rest of week 2

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nat = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.team = dict['team']
+        self.total = self.goals + self.assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20}{self.team:5}{self.goals:2} + {self.assists:2} = {self.total}"
